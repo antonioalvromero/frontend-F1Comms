3161bcc1e02b45cc00570dc55e296cf26b0f9d2d
antonioalvromero <antonio.alvarezromero@ucr.ac.cr>
2025-12-11 16:27:49 -0600

    s

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const REPO_NAME = 'frontend-F1Comms';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/${REPO_NAME}/`,
 })
